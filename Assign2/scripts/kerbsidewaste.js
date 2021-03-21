fe3b93fb915c13104b84910cd2d194c764acbc78
bmouritz <39233162+bmouritz@users.noreply.github.com>
2021-03-21 12:45:14 +1100

    updates

--- Assign2/scripts/kerbsidewaste.js
+++ Assign2/scripts/kerbsidewaste.js
@@ -1,65 +1,82 @@
 function init(){
-    var w = 1000;
+    var w = 800;
     var h = 300;
-	var padding = 20;
-    var xPadding = 30;
     var yPadding = 20;
-    
-	var dataset;
+    var xPadding = 60;
 
-	var svg = d3.select("#wasteChart").append("svg").attr("width", w).attr("height", h);
-    
-    d3.csv("./data/kerbsidevicdata2.csv").then(function(data){
-        dataset = data.map(function(d) { return [+d["Years"], +d["Total_Annual_tonnes_Collected"], +d["Total_Annual_Tonnes_Processed"]];});
-	
-		var xScale = d3.scaleLinear()
-						.domain([d3.min(dataset, function(d){
-							return d[0];
-						}),
-						d3.max(dataset, function(d){
-							return d[0];
-						})])
-						.range([xPadding, w - xPadding]);
+    var dataset;
 
-		var yScale = d3.scaleLinear()
-						.domain([d3.min(dataset, function(d){
-							return d[1];
-						}),
-						d3.max(dataset, function(d){
-							return d[1];
-						})])
-						.range([h - yPadding, yPadding]);
-		
-		var xAxis = d3.axisBottom().ticks(20).scale(xScale);
-		var yAxis = d3.axisLeft().ticks(5).scale(yScale);
-		
-		svg.selectAll("circle").data(dataset).enter().append("circle")
-			.attr("cx", function(d, i) {
-					return xScale(d[0]);
-			})
-			.attr("cy", function(d){
-				return yScale(d[1]);
-			})
-			.attr("r", 5)
-			.attr("fill", function(d){
-				return "grey";                
-		});
-		
-		svg.append("g").attr("transform", "translate(0, "+ (h - padding) +")").call(xAxis);
-		svg.append("g").attr("transform", "translate(" + 30 + ", 0)").call(yAxis);
-		
-       }).catch(function(error){
-        d3.select("svg")
-         .append("text")
-         .text("Couldn't load data")
-         .attr("x", function(d){
-           return w / 2;
-         })
-         .attr("y", function(d){
-           return h / 2;
-         })
-         .style('fill', 'red');
+    var svg = d3.select("#wasteChart")
+    .append("svg")
+    .attr("width", w)
+    .attr("height", h);
+
+    var rowConvertor = function(d){
+      return {
+        date: new Date(+d.year, 0),
+        total_collected: parseInt(+d.total_annual_tonnes_collected),
+		total_processed: parseInt(+d.total_annual_tonnes_processed)
+      };
+    }
+    
+	d3.csv("./data/victoriawaste.csv", rowConvertor, function(data){
+		dataset = data;
+    	lineChart(dataset);
+		legend();
     });
+      
+    function lineChart(dataset){
+		console.log(dataset);
+
+      var xScale = d3.scaleTime()
+        .domain([
+          d3.min(dataset, function(d){ return d.date; }),
+          d3.max(dataset, function(d){ return d.date; })
+        ])
+        .range([xPadding, w]);
+
+      var yScale = d3.scaleLinear()
+        .domain([0, d3.max(dataset, function(d){ return d.total_collected })
+        ]).range([h - yPadding, 0]);
+
+      var line = d3.line()
+        .x(function(d){return xScale(d.date);})
+        .y(function(d){return yScale(d.total_collected);});
+
+	  var line2 = d3.line()
+        .x(function(d){return xScale(d.date);})
+        .y(function(d){return yScale(d.total_processed);});
+  
+      svg.append("path")
+        .datum(dataset)
+        .attr("class", "line")
+        .attr("d", line);
+	
+	  svg.append("path")
+        .datum(dataset)
+        .attr("class", "line2")
+        .attr("d", line2);
+
+      var xAxis = d3.axisBottom().ticks(18).scale(xScale);
+      var yAxis = d3.axisLeft().scale(yScale);
+
+      svg.append("g")
+        .attr("transform", "translate (0, " + (h - yPadding) + ")")
+        .attr("class", "axis")
+        .call(xAxis);
+
+      svg.append("g")
+        .attr("transform", "translate (" + xPadding + ", 0)")
+        .attr("class", "axis")
+        .call(yAxis);
+    }
+
+	function legend(){
+		svg.append("circle").attr("cx",200).attr("cy",130).attr("r", 6).style("fill", "#69b3a2");
+		svg.append("circle").attr("cx",200).attr("cy",160).attr("r", 6).style("fill", "#404080");
+		svg.append("text").attr("x", 220).attr("y", 130).text("Victorian Waste Collected").style("font-size", "15px").attr("alignment-baseline","middle");
+		svg.append("text").attr("x", 220).attr("y", 160).text("Victorian Waste Processed").style("font-size", "15px").attr("alignment-baseline","middle");
+	}
 }
 
 window.onload = init;
