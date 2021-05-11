017d7e28e84e12f3112d7fc17b0c030822ea85d0
bmouritz <39233162+bmouritz@users.noreply.github.com>
2021-05-11 15:06:24 +1000

    axis labels added

--- Assign2/Bar Chart - Ben/scripts/barchart.js
+++ Assign2/Bar Chart - Ben/scripts/barchart.js
@@ -3,9 +3,10 @@ function init(){
     var h = 500;
     var padding = 20;
     var sortOrder = false;
-    var dataset;
+    var dataset = [0,1,2,3,4,5,6,7];
     var allYears;
 
+    /*
     d3.csv("../data/Australia Waste.csv").then(function(data){
       allYears = d3.nest()
         .key(function(d) { return d.Year; })
@@ -24,7 +25,7 @@ function init(){
         var WA = data[i].WA;
       }
   });
-
+*/
 		var xScale = d3.scaleBand()
 		  .domain(d3.range(dataset.length))
 		  .rangeRound([0, w])
@@ -47,6 +48,7 @@ function init(){
           var xPos = parseFloat(d3.select(this).attr("x")) + parseFloat(d3.select(this).attr("width"))/2 - 10;
           var yPos = parseFloat(d3.select(this).attr("y")) + 20;
           
+          // Position the tooltip
           d3.select("#tooltip") 
             .style("left", xPos + "px") 
             .style("top", yPos + "px") 
@@ -88,27 +90,46 @@ function init(){
 
     var svg = d3.select("#chart")
       .append("svg")
-      .attr("width", w + padding)
-      .attr("height", h + padding + 10)
-      .attr("transform", "translate(" + padding + "," + padding + ")");
+      .attr("width", w + 80)
+      .attr("height", h + 80)
+      .attr("transform", "translate(" + padding + "," + 80 + ")");
 
     svg.selectAll("rect")
       .data(dataset)
       .enter()
       .append("rect")
       .attr("x", function(d, i) {
-        return xScale(i) + 20;
+        return xScale(i) + 40;
       })
       .attr("y", function(d) {
-        return yScale(d) + 10;
+        return yScale(d) - 10;
       })
       .attr("width", xScale.bandwidth())
       .attr("height", function(d) {
         return h - yScale(d);
     })
 
-    svg.append("g").attr("transform", "translate(20, "+ (h + 10) +")").call(xAxis);
-    svg.append("g").attr("transform", "translate(" + 20 + ", 10)").call(yAxis);
+    // Adding X and Y axis.
+    svg.append("g").attr("transform", "translate(40, "+ (h - 10) +")").call(xAxis);
+    svg.append("g").attr("transform", "translate(" + 40 + ", -10)").call(yAxis);
+
+    // Adding Y axis label
+    svg.append("text")
+      .attr("id", "ylabel")
+      .attr("text-anchor", "middle")
+      .attr("y", 0)
+      .attr("x", -h / 2)
+      .attr("dy", ".75em")
+      .attr("transform", "rotate(-90)")
+      .text("Tonnes of Waste");
+
+    // Adding X axis label
+    svg.append("text")
+      .attr("id", "xlabel")
+      .attr("text-anchor", "middle")
+      .attr("x", 50 + w / 2)
+      .attr("y", h + 25)
+      .text("State");
 
     HoverOn();
 
