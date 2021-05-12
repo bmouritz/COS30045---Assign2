8d92a4a79fba069422a78bf1a4a74205fd556730
bmouritz <39233162+bmouritz@users.noreply.github.com>
2021-05-12 20:34:23 +1000

    dropdown filter working

--- Assign2/Bar Chart - Ben/scripts/barchart.js
+++ Assign2/Bar Chart - Ben/scripts/barchart.js
@@ -1,4 +1,8 @@
-// get it drawing on dropdown changes.
+// Change yscale based on filter of year & waste type
+// Create agg for all waste by year
+// Create agg for all year by waste
+// Fix format of hover
+// Fix y scale amounts
 
 function init(){
   var w = 700;
@@ -12,16 +16,6 @@ function init(){
   var yearSelected = document.getElementById("year").value;
   var wasteSelected = document.getElementById("wasteType").value;
   var rollup;
-
-  // Get the value of Year dropdown dynamically.
-  document.getElementById("year").addEventListener('change', function() {
-    yearSelected = this.value;
-  });
-
-  // Get the value of Waste Type dropdown dynamically.
-  document.getElementById("wasteType").addEventListener('change', function() {
-    wasteSelected = this.value;
-  });
 
   var svg = d3.select("#chart")
     .append("svg")
@@ -65,91 +59,201 @@ function init(){
       .append('option')
       .text(function (d) { return d; }) // text showed in the menu
       .attr("value", function (d) { return d; }) // corresponding value returned by the button
+      
+    var drawAll = function(){
+        if (yearSelected == "All" && wasteSelected == "All"){
+          d3.selectAll("svg > *").remove();
+      
+          xScale = d3.scaleBand()
+            .domain(["ACT","NSW", "NT", "QLD", "SA", "TAS", "VIC", "WA"])
+            .range([0, w])
+            .paddingInner(0.05);
+      
+          yScale = d3.scaleLinear()
+            .domain([0, d3.max(rollup, function(d){ return d.value})])
+            .range([h, 0]);
+      
+          svg.selectAll("rect")
+            .data(rollup)
+            .enter()
+            .append("rect")
+            .attr("x", function(d) {
+              return xScale(d.key) + 100;
+            })
+            .attr("y", function(d) {
+              return yScale(d.value) - 10;
+            })
+            .attr("width", xScale.bandwidth())
+            .attr("height", function(d) {
+              return h - yScale(d.value);
+            })
+      
+            var xAxis = d3.axisBottom().ticks(5).scale(xScale);
+            var yAxis = d3.axisLeft().scale(yScale);
+      
+          // Adding X and Y axis.
+          svg.selectAll("#yaxis").remove();
+          svg.selectAll("#xaxis").remove();
+          svg.append("g").attr("id", "xaxis").attr("transform", "translate(100, "+ (h - 10) +")").call(xAxis);
+          svg.append("g").attr("id", "yaxis").attr("transform", "translate(" + 100 + ", -10)").call(yAxis); 
 
-
-  if (yearSelected == "All" && wasteSelected == "All"){
-    d3.selectAll("svg > *").remove();
-
-    xScale = d3.scaleBand()
-      .domain(["ACT","NSW", "NT", "QLD", "SA", "TAS", "VIC", "WA"])
-      .range([0, w])
-      .paddingInner(0.05);
-
-    yScale = d3.scaleLinear()
-      .domain([0, d3.max(rollup, function(d){ return d.value})])
-      .range([h, 0]);
-
-    svg.selectAll("rect")
-      .data(rollup)
-      .enter()
-      .append("rect")
-      .attr("x", function(d) {
-        return xScale(d.key) + 100;
-      })
-      .attr("y", function(d) {
-        return yScale(d.value) - 10;
-      })
-      .attr("width", xScale.bandwidth())
-      .attr("height", function(d) {
-        return h - yScale(d.value);
-      })
-
-      var xAxis = d3.axisBottom().ticks(5).scale(xScale);
-      var yAxis = d3.axisLeft().scale(yScale);
-
-    // Adding X and Y axis.
-    svg.append("g").attr("transform", "translate(100, "+ (h - 10) +")").call(xAxis);
-    svg.append("g").attr("transform", "translate(" + 100 + ", -10)").call(yAxis); 
-
-    // Adding Y axis label
-    svg.append("text")
-      .attr("id", "ylabel")
-      .attr("text-anchor", "middle")
-      .attr("y", 0)
-      .attr("x", -h / 2)
-      .attr("dy", ".75em")
-      .attr("transform", "rotate(-90)")
-      .text("Tonnes of Waste");
-
-    // Adding X axis label
-    svg.append("text")
-      .attr("id", "xlabel")
-      .attr("text-anchor", "middle")
-      .attr("x", 50 + w / 2)
-      .attr("y", h + 25)
-      .text("State");
-
-    HoverOn();
-  }  });
-};
-// Hover effects and tooltips
-var HoverOn = function(){
-  svg.selectAll("rect")
-  .on("mouseover", function(d, i){
-    var xPos = parseFloat(d3.select(this).attr("x")) + parseFloat(d3.select(this).attr("width"))/2 - 10;
-    var yPos = parseFloat(d3.select(this).attr("y")) + 20;
+          // Adding Y axis label
+          svg.append("text")
+            .attr("id", "ylabel")
+            .attr("text-anchor", "middle")
+            .attr("y", 0)
+            .attr("x", -h / 2)
+            .attr("dy", ".75em")
+            .attr("transform", "rotate(-90)")
+            .text("Tonnes of Waste");
+      
+          // Adding X axis label
+          svg.append("text")
+            .attr("id", "xlabel")
+            .attr("text-anchor", "middle")
+            .attr("x", 50 + w / 2)
+            .attr("y", h + 25)
+            .text("State");
+      
+          HoverOnDerived();
+        } else {
+          d3.selectAll("svg > *").remove();
+      
+          xScale = d3.scaleBand()
+            .domain(["ACT","NSW", "NT", "QLD", "SA", "TAS", "VIC", "WA"])
+            .range([0, w])
+            .paddingInner(0.05);
           
-    // Position the tooltip
-    d3.select("#tooltip") 
-      .style("left", xPos + "px") 
-      .style("top", yPos + "px") 
-      .select("#value") 
-      .text(d.value); 
+          // add filter to scale//
+          yScale = d3.scaleLinear()
+            .domain([0, d3.max(dataset, function(d){ return d.Tonnes})])
+            .range([h, 0]);
+      
+          svg.selectAll("rect")
+            .data(dataset)
+            .enter()
+            .append("rect")
+            .filter(function(d) { return d.Category == wasteSelected })
+            .attr("x", function(d) {
+              return xScale(d.Jurisdiction) + 100;
+            })
+            .attr("y", function(d) {
+              return yScale(d.Tonnes) - 10;
+            })
+            .attr("width", xScale.bandwidth())
+            .attr("height", function(d) {
+              return h - yScale(d.Tonnes);
+            })
 
-    // Position the tooltip
-    d3.select("#tooltip") 
-      .select("#state") 
-      .text(d.key);
-            
-    //Show the tooltip 
-    d3.select("#tooltip")
-      .classed("hidden", false ); 
-  }) 
-  .on("mouseout", function () { 
-    //Hide the tooltip 
-    d3.select("#tooltip")
-      .classed("hidden", true );
+            svg.selectAll("rect")
+            .filter(function(d) { return d.Year == yearSelected })
+      
+            var xAxis = d3.axisBottom().ticks(5).scale(xScale);
+            var yAxis = d3.axisLeft().scale(yScale);
+      
+          // Adding X and Y axis.
+          svg.selectAll("#yaxis").remove();
+          svg.selectAll("#xaxis").remove();
+          svg.append("g").attr("transform", "translate(100, "+ (h - 10) +")").call(xAxis);
+          svg.append("g").attr("transform", "translate(" + 100 + ", -10)").call(yAxis); 
+          
+          svg.selectAll("g.y.axis")
+          .call(yAxis);
+
+          // Adding Y axis label
+          svg.append("text")
+            .attr("id", "ylabel")
+            .attr("text-anchor", "middle")
+            .attr("y", 0)
+            .attr("x", -h / 2)
+            .attr("dy", ".75em")
+            .attr("transform", "rotate(-90)")
+            .text("Tonnes of Waste");
+      
+          // Adding X axis label
+          svg.append("text")
+            .attr("id", "xlabel")
+            .attr("text-anchor", "middle")
+            .attr("x", 50 + w / 2)
+            .attr("y", h + 25)
+            .text("State");
+      
+          HoverOn();
+        }
+      }
+      drawAll();
+
+      // Get the value of Year dropdown dynamically.
+      document.getElementById("year").addEventListener('change', function() {
+        yearSelected = this.value;
+        drawAll();
+      });
+
+      // Get the value of Waste Type dropdown dynamically.
+      document.getElementById("wasteType").addEventListener('change', function() {
+        wasteSelected = this.value;
+    });
   });
+
+  // Hover effects and tooltips
+  var HoverOnDerived = function(){
+    svg.selectAll("rect")
+    .on("mouseover", function(d, i){
+      var xPos = parseFloat(d3.select(this).attr("x")) + parseFloat(d3.select(this).attr("width"))/2 - 10;
+      var yPos = parseFloat(d3.select(this).attr("y")) + 20;
+            
+      // Position the tooltip
+      d3.select("#tooltip") 
+        .style("left", xPos + "px") 
+        .style("top", yPos + "px") 
+        .select("#value") 
+        .text(d.value); 
+
+      // Position the tooltip
+      d3.select("#tooltip") 
+        .select("#state") 
+        .text(d.key);
+              
+      //Show the tooltip 
+      d3.select("#tooltip")
+        .classed("hidden", false ); 
+    }) 
+    .on("mouseout", function () { 
+      //Hide the tooltip 
+      d3.select("#tooltip")
+        .classed("hidden", true );
+    });
   };
+
+    // Hover effects and tooltips
+    var HoverOn = function(){
+      svg.selectAll("rect")
+      .on("mouseover", function(d, i){
+        var xPos = parseFloat(d3.select(this).attr("x")) + parseFloat(d3.select(this).attr("width"))/2 - 10;
+        var yPos = parseFloat(d3.select(this).attr("y")) + 20;
+              
+        // Position the tooltip
+        d3.select("#tooltip") 
+          .style("left", xPos + "px") 
+          .style("top", yPos + "px") 
+          .select("#value") 
+          .text(d.Tonnes); 
+  
+        // Position the tooltip
+        d3.select("#tooltip") 
+          .select("#state") 
+          .text(d.Jurisdiction);
+                
+        //Show the tooltip 
+        d3.select("#tooltip")
+          .classed("hidden", false ); 
+      }) 
+      .on("mouseout", function () { 
+        //Hide the tooltip 
+        d3.select("#tooltip")
+          .classed("hidden", true );
+      });
+    };
+};
 
 window.onload = init;
