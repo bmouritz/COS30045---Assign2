56dcf6c17295d77916479c4a881a3bb414753cbb
Shane <drumbastic@hotmail.com>
2021-05-17 14:47:58 +1000

    legend

--- Assign2/Global Choropleth/scripts/global_generate_map.js
+++ Assign2/Global Choropleth/scripts/global_generate_map.js
@@ -37,27 +37,55 @@ function generateMap(dataString, countryColour) {
     .style("opacity", 0.8);
 
   //Not yet working - keep chipping away at
-  var legend = svg.selectAll('rect')
-    .data(countryColour.domain().reverse())
-    .enter()
-    .append('rect')
-    .attr("x", w - 780)
-    .attr("y", function(d, i) {
-      return i * 20;
-    })
-    .attr("width", 10)
-    .attr("height", 10)
-    .style("fill", countryColour);
+  // var legend = svg.selectAll('#legendChart')
+  //   .data(countryColour.domain().reverse())
+  //   .enter()
+  //   .append('rect')
+  //   .attr("x", 0)
+  //   .attr("y", function(d, i) {
+  //     return i * 20;
+  //   })
+  //   .attr("width", 10)
+  //   .attr("height", 10)
+  //   .style("fill", countryColour);
 
   //Load in Country data from csv
   d3.csv(dataString, (error, data) => {
     if (error) return console.error(error); // Throw error if csv not found
 
+    // Set min and max
+    var min = d3.min(data, function(d) { return d.AMOUNT; });
+    var max = d3.max(data, function(d) { return d.AMOUNT; });
+
     //Set input domain for colour scale dynamically
-    countryColour.domain([
-      d3.min(data, function(d) { return d.AMOUNT; }),
-      d3.max(data, function(d) { return d.AMOUNT; })
-    ]);
+    countryColour.domain([min,max]);
+
+    // Legend
+    var categories = [min,max];
+    // var ordinal = d3.scaleOrdinal()
+	  //   .domain(categories)
+  	//   .range(categories.map((val, i) =>
+		// 		d3.interpolateYlGnBu(i / (categories.length - 1))
+		// 	));
+    // var legendOrdinal = d3.legendColor().scale(ordinal);
+    // svg.select("#legendChart").call(legendOrdinal);
+
+    var legend = svg.select('#legendChart')
+    .append("svg")
+      .attr("width", 960)
+      .attr("height", 250);
+
+    legend.append("g")
+      .attr("class", "legendInterval")
+      .attr("transform", "translate(20,20)");
+
+    var linear = d3.scaleLinear()
+      .domain(categories)
+      .range(categories);
+    var legendLinear = d3.legendColor().scale(linear);
+
+    legend.select("#legendChart")
+      .call(legendLinear);
 
     //Load in GeoJSON data
     d3.json("./data/countries.geojson", (error, json) => {
@@ -84,8 +112,6 @@ function generateMap(dataString, countryColour) {
         }
       }
 
-      var selected = '';
-
       //Bind data and create one path per GeoJSON feature
       svg.selectAll("path")
         .data(json.features)
@@ -109,7 +135,7 @@ function generateMap(dataString, countryColour) {
             .style("stroke", "black")
             .style("stroke-width", 0.10);
 
-          //Hide the tooltip 
+          //Hide the tooltip
           d3.select("#tooltip")
             .classed("hidden", true );
         })
@@ -119,22 +145,22 @@ function generateMap(dataString, countryColour) {
           // Getting data variables
           selected = event.properties.ADMIN;
           amount = event.properties.AMOUNT;
-          
+
           // Set x,y of tooltip and set text of country
-          d3.select("#tooltip") 
+          d3.select("#tooltip")
             .style('left', mouse[0] + `px`) // Use mouse x coordinates to draw text box
             .style('top', (mouse[1] + 20) + `px`) // Use mouse y coordinates to draw text box
-            .select("#country") 
-            .text(selected); 
+            .select("#country")
+            .text(selected);
 
           // Set text of the amount
           d3.select("#tooltip")
             .select("#value")
             .text(amount);
 
-          //Show the tooltip 
+          //Show the tooltip
           d3.select("#tooltip")
-            .classed("hidden", false ); 
+            .classed("hidden", false );
         })
         .style("fill", function(d) {
           var AMOUNT = d.properties.AMOUNT; //Get data AMOUNT
