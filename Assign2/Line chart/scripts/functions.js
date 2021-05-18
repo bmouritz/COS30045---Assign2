f398dc3191dea56e78498aebd663a1c5342d6265
bmouritz <39233162+bmouritz@users.noreply.github.com>
2021-05-18 12:41:01 +1000

    Line chart added GDP data and padding on graph

--- Assign2/Line chart/scripts/functions.js
+++ Assign2/Line chart/scripts/functions.js
@@ -1,6 +1,6 @@
 // Create Line Chart
-function lineChart(dataset) {
-  var w = 615;
+function lineChart(dataset, dataset1) {
+  var w = 700;
   var h = 300;
   var padding = 40;
 
@@ -9,10 +9,15 @@ function lineChart(dataset) {
     .rollup(function(v) { return d3.sum(v, function(d) { return d.Tonnes; }); })
     .entries(dataset);
 
-  console.log(dataset);
+  dataset1 = d3.nest()
+    .key(function(d) { return d.Year; })
+    .rollup(function(v) { return d3.sum(v, function(d) { return d.Amount; }); })
+    .entries(dataset1);
+
+  console.log(dataset1);
   
   xScale = d3.scaleTime()
-  .domain(d3.extent(dataset, function(d) { 
+  .domain(d3.extent(dataset1, function(d) { 
     return new Date(d.key); 
   }))
   .range([padding, w-29]);
@@ -21,33 +26,35 @@ function lineChart(dataset) {
   .domain([0, d3.max(dataset, function(d) { return d.value; })])
   .range([h - padding, 0]);
 
- /* y2Scale = d3.scaleLinear()
-  .domain([0, d3.max(dataset, function(d) { return d.billions; })])
-  .range([h - padding, 0]);*/
+  y2Scale = d3.scaleLinear()
+  .domain([0, d3.max(dataset1, function(d) { return d.value; })])
+  .range([h - padding, 0]);
 
   // Set up the Lines
   y1Line = d3.line()
   .x(function(d) { return xScale(new Date(d.key)); })
   .y(function(d) { return y1Scale(d.value); });
 
-/*  y2Line = d3.line()
-  .x(function(d) { return xScale(d.year); })
-  .y(function(d) { return y2Scale(d.billions); });*/
+  y2Line = d3.line()
+  .x(function(d) { return xScale(new Date(d.key)); })
+  .y(function(d) { return y2Scale(d.value); });
 
   var svg = d3.select("#chart")
     .append("svg")
-    .attr("width",w)
+    .attr("width",w + padding + padding)
     .attr("height",h);
 
   svg.append("path")
     .datum(dataset)
     .attr("class", "y1Line")
+    .attr("transform", "translate(" + 50 + ",0)")
     .attr("d", y1Line);
 
-/*  svg.append("path")
-    .datum(dataset)
+  svg.append("path")
+    .datum(dataset1)
     .attr("class", "y2Line")
-    .attr("d", y2Line);*/
+    .attr("transform", "translate(" + 50 + ",0)")
+    .attr("d", y2Line);
 
   //Add Axis
   xAxis = d3.axisBottom()
@@ -59,28 +66,28 @@ function lineChart(dataset) {
     .scale(y1Scale)
     .ticks(10);
 
-/*  y2Axis = d3.axisRight()
+  y2Axis = d3.axisRight()
     .scale(y2Scale)
-    .ticks(10);*/
+    .ticks(10);
 
   svg.append("g")
     .attr("class", "axis")
-    .attr("transform", "translate(0," + (h - padding) + ")")
+    .attr("transform", "translate(50," + (h - padding) + ")")
     .call(xAxis);
 
   svg.append("g")
     .attr("class", "axis")
     .attr("color","green")
-    .attr("transform", "translate(" + padding + ",0)")
+    .attr("transform", "translate(" + 90 + ",0)")
     .call(y1Axis);
 
-/*  svg.append("g")
+  svg.append("g")
     .attr("class", "axis")
     .attr("color","red")
-    .attr("transform", "translate(" + (600-15) + ",0)")
-    .call(y2Axis);*/
+    .attr("transform", "translate(" + (720) + ",0)")
+    .call(y2Axis);
 
-  svg.selectAll("circle").data(dataset).enter().append("circle")
+/*  svg.selectAll("circle").data(dataset).enter().append("circle")
     .attr("cx", function(d) {
             return xScale(new Date(d.key));
     })
@@ -88,7 +95,17 @@ function lineChart(dataset) {
         return y1Scale(d.value);
     })
     .attr("r", 5)
-    .attr("fill", "red");
+    .attr("fill", "green");
+
+  svg.selectAll("circle").data(dataset1).enter().append("circle")
+    .attr("cx", function(d) {
+            return xScale(new Date(d.key));
+    })
+    .attr("cy", function(d){
+        return y2Scale(d.value);
+    })
+    .attr("r", 5)
+    .attr("fill", "red");*/
 
   // Hover effects and tooltips for derived metrics
   var HoverOn = function(){
--- Assign2/Line chart/scripts/line_chart_test.js
+++ Assign2/Line chart/scripts/line_chart_test.js
@@ -1,6 +1,7 @@
 function init() {
 
 	var dataset;
+	var dataset1;
 
   // Load in CSV file
   d3.csv("../data/Australia Waste.csv").then(function(data){
@@ -10,7 +11,20 @@ function init() {
     	dataset.forEach(function(d) { d.Tonnes = parseInt(d.Tonnes.replace(/,/g, ""))});
 		// Convert years column from String to Date
     	dataset.forEach(function(d) { d.Year = new Date(+d.Year.substr(d.Year.length - 4), 0, 1)});
-		lineChart(dataset);
+
+		// Load in second CSV file
+		  d3.csv("../data/5220001_Annual_Gross_State_Product_All_States.csv").then(function(data1){
+			dataset1 = data1;
+
+			// Convert amount column from String to Int
+    		dataset1.forEach(function(d) { d.Amount = parseInt(d.Amount.replace(/,/g, ""))});
+
+			// Convert years column from String to Date
+    		dataset1.forEach(function(d) { d.Year = new Date(+d.Year.substr(d.Year.length - 4), 0, 1)});
+
+			lineChart(dataset, dataset1);
+		})
+		
 	})
 }
 
