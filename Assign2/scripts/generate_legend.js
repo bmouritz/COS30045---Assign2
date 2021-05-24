39233f31dc54facfd3bab052bff7b1bce94246ac
bmouritz <39233162+bmouritz@users.noreply.github.com>
2021-05-24 14:23:49 +1000

    Choropleth added

---
+++ Assign2/scripts/generate_legend.js
@@ -0,0 +1,64 @@
+function generateLegend(min, max, colorScheme){
+
+  var w = 100;
+  var h = 120;
+
+  var num2 = max * 0.25; // 25% value
+  var num3 = max * 0.5;  // 50% value
+  var num4 = max * 0.75; // 75% value
+
+  // Set up formatting for labels
+  var format = d3.format("0.2s"); // Default
+  var legendLabel = "GDP ($US)"; // Default
+  var dataVisSelected = document.getElementById("viz_data").value;
+  if(dataVisSelected == "GDP"){
+    format = d3.format("0.2s");
+    legendLabel = "GDP ($US)";
+  } else if (dataVisSelected == "Waste"){
+    format = d3.format("0.2s");
+    legendLabel = "Waste (Tonnes)";
+  } else if (dataVisSelected == "Ratio"){
+    format = d3.format("0.2s");
+    legendLabel = "Ratio ($ vs W)";
+  }
+
+  // Use the above defined format to formate the legend's labals
+  var labels = [
+    format(Math.round(min)).replace(/G/,"B"),
+    format(Math.round(num2)).replace(/G/,"B"),
+    format(Math.round(num3)).replace(/G/,"B"),
+    format(Math.round(num4)).replace(/G/,"B"),
+    format(Math.round(max)).replace(/G/,"B")
+  ];
+
+  var svg = d3.select("#legend")
+    .append("svg")
+    .attr("class", "vis")
+    .attr("width",w)
+    .attr("height",h);
+
+  var countryColourQuantized = d3.scaleQuantize().range(colorScheme).domain([min,max]);
+
+    var colorScale = d3.scaleQuantize()
+        .domain([min,max])
+        .range(colorScheme);
+
+  var g = svg.append("g")
+      .attr("class", "legendThreshold")
+      .attr("transform", "translate(5,20)");
+
+  g.append("text")
+      .attr("class", "caption")
+      .attr("x", 0)
+      .attr("y", -6)
+      .style("font-weight","bold")
+      .text(legendLabel); // Set dynamically
+
+  var legend = d3.legendColor()
+      .labels(function (d) { return labels[d.i]; })
+      .shapePadding(4)
+      .scale(countryColourQuantized);
+
+  svg.select(".legendThreshold")
+      .call(legend);
+};
---
+++ Assign2/scripts/script.js
@@ -0,0 +1,90 @@
+function init() {
+
+  // var dataString = "./data/global_vis_data.csv"; // Path to data source
+  var dataString = "./data/global_vis_data - real waste data and ratio.csv";
+  var dataVisSelected = "GDP"; // Default data for visualisation
+  var minYear = 1960, maxYear = 2017; // Declare min/max years from dataset
+  var yearSelected = 2017; // Set default selected year
+  var vis_footnote_text = "GDP (USD 2010) per Country for 2017"; // Text for data visualtisation footnote
+  d3.select("#vis_footnote").text(vis_footnote_text); // Default
+
+  // Define colour schemes
+  var GdpColorScheme = [
+    "rgb(237,248,233)",
+    "rgb(186,228,179)",
+    "rgb(116,196,118)",
+    "rgb(49,163,84)",
+    "rgb(0,109,44)"
+  ];
+  var WasteColorScheme = [
+    "rgb(254,237,222)",
+    "rgb(253,190,133)",
+    "rgb(253,141,60)",
+    "rgb(230,85,13)",
+    "rgb(166,54,3)"
+  ];
+  var RatioColorScheme = [
+    "rgb(239,243,255)",
+    "rgb(189,215,231)",
+    "rgb(107,174,214)",
+    "rgb(49,130,189)",
+    "rgb(8,81,156)"
+  ];
+
+  // Default colour scheme
+  var colourScheme = GdpColorScheme;
+
+  // Reusable funtion to update the variables outlined above
+  var updateVariables = function(){
+    dataVisSelected = document.getElementById("viz_data").value;
+    if(dataVisSelected == "GDP"){
+      colourScheme = GdpColorScheme;
+      vis_footnote_text = "GDP (USD 2010) per Country for " + yearSelected;
+    } else if (dataVisSelected == "Waste"){
+      colourScheme = WasteColorScheme;
+      vis_footnote_text = "Waste output (Tonnes) per Country for " + yearSelected;
+    } else if (dataVisSelected == "Ratio"){
+      colourScheme = RatioColorScheme;
+      vis_footnote_text = "Ratio of Gross Domestic Product (USD 2010) vs. Waste Output (Tonnes) for " + yearSelected;
+    }
+    d3.select("#vis_footnote").text(vis_footnote_text);
+  };
+
+  var updateVisualisations = function(){
+    generateMap(dataString, colourScheme, yearSelected);
+  };
+
+  // Set up slider and its functionality
+  var slider = d3.sliderHorizontal()
+  .min(minYear)
+  .max(maxYear)
+  .step(1)
+  .width(600)
+  .tickFormat(d3.format("d"))
+  .default(yearSelected)
+  .displayValue(true)
+  .on('onchange', val => {
+    yearSelected = val;
+    updateVariables();
+    d3.selectAll(".vis").remove(); // Remove old visualisation
+    updateVisualisations(); // update visualisations on slider update
+  });
+  var g = d3.select("#slider").append("svg")
+  .attr("width", 700)
+  .attr("height", 120)
+  .append("g")
+  .attr("transform", "translate(30,30)")
+  .call(slider);
+
+  // Drop down menu behavior
+  document.getElementById("viz_data").addEventListener('change', function() {
+    updateVariables();
+    d3.selectAll(".vis").remove(); // Remove old visualisation
+    updateVisualisations(); // upadte visualisations on slider update
+  });
+
+  // Initial generation of visualisation
+  updateVisualisations();
+};
+
+window.onload = init;
